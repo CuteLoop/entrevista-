aeca81c49de6a6bdf04ee0633d45e065ae3902b4
Mar <joel23tanori@gmail.com>
2022-07-01 08:28:41 -0700

    finally, just needs changing format

--- cash.py
+++ cash.py
@@ -1,31 +1,39 @@
+import sys
 
 def is_non_negative(number):
 	if number >= 0:
 			return True
 	else:
-		print("Please write a non negative value for owed change.")
+		print("Please write a non negative value for owed change:")
 		return False
 		
 def is_rounded_to_cents(number):
 	if number - round(number,2) < 0.000000000001:
 		return True
 	else:
-		print("Please round change to Cents.")
+		print("Please round change to Cents:")
 		return False
 
 
+def make_float(number):
+	try:
+		number = float(number)
+	except ValueError:
+		print("Sorry, please write a number:")
+		return -1
+	return number
+	
+	
 def get_change():
 	
+	change_owed = make_float(sys.argv[1])
 	while True:
-		try:
-			change_owed = float(input('Please type how much change is owed, rounded to cents.'))
-		except ValueError:
-			print("Sorry, please write a number")
-			
 		if is_non_negative(change_owed) and is_rounded_to_cents(change_owed) ==True:
+		
 			break
-			
-	return change_owed		
+		change_owed = make_float((input('Please type how much change is owed, rounded to cents:')))
+	
+	return round(100*change_owed, 2)		
 	
 
 def min_number_of_coins():
@@ -49,4 +57,5 @@ def min_number_of_coins():
 
 number = min_number_of_coins()
 
+#print(make_float(''))
 print(number)
