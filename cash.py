64267b693034507eab195161e28c61d37eae24c4
Mar <joel23tanori@gmail.com>
2022-07-01 09:22:04 -0700

    input done

--- cash.py
+++ cash.py
@@ -1,7 +1,6 @@
 import sys
 
 def is_non_negative(number):
-	print(f"non neg {number}")
 	if number >= 0:
 			return True
 	else:
@@ -9,8 +8,8 @@ def is_non_negative(number):
 		return False
 		
 def is_rounded_to_cents(number):
-	print(f"rounding {number - round(number,2)}")
-	if number - round(number,2) >= 0:
+
+	if abs(number - round(number,2)) ==0:
 		return True
 	else:
 		print("Please round change to Cents:")
@@ -47,41 +46,39 @@ def get_change():
 	Re-prompt if user fails to provide a non negative number
 	or an amount that is not rounded to cents.
 	"""
-
 	if is_float(sys.argv[1]):
 		change_owed = float(sys.argv[1])
 	
 	while True:
-	
 		if is_non_negative(change_owed) and is_rounded_to_cents(change_owed) ==True:
-			break
-			
+			break	
 		change_owed = get_float_input()
-		
-	
+
 	return round(100*change_owed, 2)		
 	
 
-def min_number_of_coins():
+
+def min_number_of_coins(change_owed):
 
 	coin_values = [25,10,5,1]
-	change_owed = int(get_change())
+	change_owed =int(change_owed)
 
 
 	coin_counter=0
 	for coin in coin_values:
 		if change_owed/coin > 0:
-			print(f'coin counter:{coin_counter} change owed {change_owed}')
+		#print(f'coin counter:{coin_counter} change owed {change_owed}')
 			coin_counter+= change_owed//coin
 			change_owed = change_owed%coin
-	print(f'coin counter:{coin_counter} change owed {change_owed}')	
+	#prnt(f'coin counter:{coin_counter} change owed {change_owed}')	
 	return coin_counter
 	
 	
 #min_number_of_coins()
 #cents = get_cents_owed()		
 
-number = min_number_of_coins()
 
-#print(make_float(''))
+change_owed = int(get_change())
+number = min_number_of_coins(change_owed)
+
 print(number)
